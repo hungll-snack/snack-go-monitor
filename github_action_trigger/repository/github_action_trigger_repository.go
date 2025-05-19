a4cb9431af6e19bbe0a8c2b1bfa11f3c8dfedefa
ajeseung <ajeseung@naver.com>
2025-05-19 16:26:25 +0900

    feat:[손승일] trigger에서 branch 적용할 수 있게 수정

--- github_action_trigger/repository/github_action_trigger_repository.go
+++ github_action_trigger/repository/github_action_trigger_repository.go
@@ -1,5 +1,5 @@
 package repository
 
 type GitHubActionTriggerRepository interface {
-	TriggerWorkflow(repoUrl string, token string, workflowFileName string) error
+	TriggerWorkflow(repoUrl string, token string, workflowFileName string, ref string) error
 }
